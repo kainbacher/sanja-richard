b9c99679182686564bc8c4c4ca031a1cf9460f72
kainbacher <rka@beautyparlour.at>
2022-12-14 21:20:07 +0100

    first shot

---
+++ components/global/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-gray-600 body-font bg-cyan-600">
+    <div class="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <nav
+        class="md:mr-auto md:ml-4 md:pl-4 flex flex-wrap items-center text-base justify-center"
+      >
+        <nuxt-link to="/termine" class="p-5 text-white hover:text-cyan-100 hover:bg-cyan-700">
+          Termine
+        </nuxt-link>
+        <nuxt-link to="/biografie" class="p-5 text-white hover:text-cyan-100 hover:bg-cyan-700">
+          Biografie
+        </nuxt-link>
+        <nuxt-link
+          to="/fotos-und-hoerbeispiele"
+          class="p-5 text-white hover:text-cyan-100 hover:bg-cyan-700"
+        >
+          Fotos & Hörbeispiele
+        </nuxt-link>
+        <nuxt-link to="/kontakt" class="p-5 text-white hover:text-cyan-100 hover:bg-cyan-700">
+          Kontakt
+        </nuxt-link>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a.nuxt-link-active {
+  background-color: #06b6d4;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,94 @@
 <template>
   <main>
-    <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+    <section id="start" class="text-gray-600 h-screen">
+      <div class="container px-5 pt-8 pb-24 mx-auto flex flex-col">
+        <h1>Alles, was du über unsere Hochzeit wissen musst</h1>
+      </div>
     </section>
 
-    <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
+    <section id="ablauf" class="text-gray-600 h-screen bg-red-50">
+      <div class="container px-5 pt-8 pb-24 mx-auto flex flex-col">
+        <h2>ABLAUF/ TIMING</h2>
+        <ul class="uppercase">
+          <li>15:30 UHR Kirchliche Trauung</li>
+          <li>16:30 UHR Agabpe</li>
+          <li>19:00 Uhr Speis und Trank</li>
+          <li>Anschließend Feierlichkeiten</li>
+        </ul>
+      </div>
+    </section>
+
+    <section id="location" class="text-gray-600 h-screen">
+      <div class="container px-5 pt-8 pb-24 mx-auto flex flex-col">
+        <h1>Location</h1>
+        <p>
+          Wir feiern mit besonderem Ambiente inmitten grüner Wiesen, Biotop und Badeteich.
+          Eine außergewöhnlicheLocationmitprächtigem Ausblick in der Nähe des Golfplatzes
+          in Rankweil.
+        </p>
+        <p>
+          GSTACH -isch sau wohl fühlen<br />
+          Großfeldweg 11<br />
+          A 6830 Rankweil <br />
+          <a href="www.gstach.at" target="_blank" title="Gstach">www.gstach.at</a>
+        </p>
+      </div>
+    </section>
+
+    <section id="organisation" class="text-gray-600 h-screen bg-yellow-100">
+      <div class="container px-5 pt-8 pb-24 mx-auto flex flex-col">
+        <h1>Organisation</h1>
+        <p>
+          Wenn Ihr unseren Tag mitgestalten möchtet, meldet Euch bitte bei unseren
+          Trauzeugen.
+        </p>
+        <p>
+          Klara Schwarz<br />
+          +43 (0) 650 333 37 83<br />
+        </p>
+        <p>
+          Benedikt Sabato<br />
+          +43 (0) 699 17 22 28 32<br />
+        </p>
+        <p>
+          Liebe Eltern,<br />
+          wir möchten diesen Tag ausgelassen mit euch feiern, darum bitten wir euch aus-
+          nahmsweise, ohne eure Kinder zu erscheinen.
+        </p>
+        <p>Um Zu- oder Absage bitten wir bis 15. April 2023.</p>
+        <p>Sanja +43 (0) 650 333 37 83</p>
+        <p>Richard +43 (0) 699 17 22 28 32</p>
+      </div>
+    </section>
+
+    <section id="sleep" class="text-gray-600 h-screen">
+      <div class="container px-5 pt-8 pb-24 mx-auto flex flex-col">
+        <h1>Übernachtung</h1>
+        <p>
+          Übernachtungsmöglichkeiten sind im Hotel Firmament vorhanden, bitte verwendet
+          für eure Buchung unseren Hochzeits-Code.
+        </p>
+        <p class="text-yellow-500">Code XXX</p>
+        <p>
+          Firmament<br />
+          Römergrund 1<br />
+          A 6830 Rankweil<br />
+          <a href="www.firmament.at" target="_blank" title="Firmament"
+            >www.firmament.at</a
+          >
+        </p>
+      </div>
+    </section>
+
+    <section id="present" class="text-gray-600 h-screen bg-green-300">
+      <div class="container px-5 pt-8 pb-24 mx-auto flex flex-col">
+        <h1>Geschenk</h1>
+        <p>
+          Unser größtes Geschenk ist es, unseren Tag mit Euch zu feiern. Falls Ihr uns
+          darüber hinaus noch beschenken möchtet, freuen wir uns über einen Beitrag für
+          unseren geplanten Familien - Hochzeitsreise.
+        </p>
+      </div>
     </section>
   </main>
 </template>
